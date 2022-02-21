3e69481c79f1c605505de2b0734079b25ad75960
Keaton Robinson <keatonr06@gmail.com>
2022-02-21 13:46:45 -0800

    completely switched to statically served SPA

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -5,26 +5,6 @@ import express from 'express';
 
 const server = express();
 server.use(express.static('dist'));
-
-server.get('/', (req, res) => {
-  // could switch to static file serving, perhaps even with initial react components rendered already and using react's hydrate on the client side
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>X-Wing Squad Builder</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="fonts/xwing-miniatures.css">
-        <link rel="stylesheet" href="styles/backgrounds.css">
-        <link rel="stylesheet" href="styles/styles.css">
-        <script src="https://kit.fontawesome.com/e5814cf8ca.js" crossorigin="anonymous"></script>
-      </head>
-      <body>
-        <div id="mountNode"></div>
-        <script src="/main.js"></script>
-      </body>
-    </html>
-  `)
-});
+// could switch to static file serving, perhaps even with initial react components rendered already and using react's hydrate on the client side
 
 server.listen(4242, () => console.log('Server is running...'));
