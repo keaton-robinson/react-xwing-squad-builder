ec33a091625c577dee287cbc0dd291aa0a6ff7ad
Keaton Robinson <keatonr06@gmail.com>
2022-03-30 03:16:13 -0700

    moved modal control to App component from SquadBuilderCpt

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import HeaderComponent from './HeaderComponent.js';
 import SquadBuilderCpt from './SquadBuilderCpt.js';
+import ModalContainer from './modals/ModalContainer.js';
 import * as xwingData from '../data/xwing_data';
 
 export default class App extends React.Component  {
 
-  state = { selectedFaction: xwingData.factionNames['Rebel Alliance'] }
+  state = { selectedFaction: xwingData.factionNames['Rebel Alliance'], modalToShow: null }
 
   factionsOrdered = [
     xwingData.factionNames["Rebel Alliance"],
@@ -17,6 +18,18 @@ export default class App extends React.Component  {
     xwingData.factionNames["Separatist Alliance"]
     //,"MultiFaction"
   ];
+
+  setModal = ( modalConfig ) => {
+    const state = this.state;
+
+    const modalToShow = modalConfig ? <ModalContainer handleClose={() => this.setModal(null)} 
+                        headerTitle={modalConfig.title}
+                        children={modalConfig.children}/> : null;
+
+    if(state.modalToShow != modalToShow){
+      this.setState({ ...state, modalToShow: modalToShow });
+    }
+  }
 
   constructor(props){
     super(props);
@@ -32,16 +45,19 @@ export default class App extends React.Component  {
         <HeaderComponent 
           factions={this.factionsOrdered} 
           selectedFaction={this.state.selectedFaction}
-          onClick={this.setSelectedFaction} 
+          onClick={this.setSelectedFaction}
+          setModal={this.setModal} 
         />
         <main>
           {this.factionsOrdered.map(faction=> (
             <SquadBuilderCpt
             selectedFaction={this.state.selectedFaction} 
             key={faction} 
-            faction={faction} />
+            faction={faction}
+            setModal={this.setModal} />
           ))}
         </main>
+        { this.state.modalToShow && this.state.modalToShow }
       </div>
     );
   }
