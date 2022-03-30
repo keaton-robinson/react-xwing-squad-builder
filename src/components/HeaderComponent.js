77bd1262948c49d192d8cc17e1a32bdd9cf8f7b3
Keaton Robinson <keatonr06@gmail.com>
2022-03-29 18:32:10 -0700

    added login button and github link

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -11,15 +11,21 @@ export default class HeaderComponent extends React.Component {
     return (
       <div>   
         <header id="navbar">
-            <nav id="factionChooser">
-                <ul>
-                {this.props.factions.map(faction => (
-                    <FactionCpt faction={ faction } key={ faction } selectedFaction={ this.props.selectedFaction }
-                      onClick={this.props.onClick}
-                    />
-                  ))}
-                </ul>
-            </nav>
+          <nav id="factionChooser">
+            <ul>
+            {this.props.factions.map(faction => (
+                <FactionCpt faction={ faction } key={ faction } selectedFaction={ this.props.selectedFaction }
+                  onClick={this.props.onClick}
+                />
+              ))}
+            </ul>
+          </nav>
+          <nav id="navLinks">
+              <ul>
+                <li><a href="https://github.com/keaton-robinson/react-xwing-squad-builder">GitHub Repository</a></li>
+                <li id="loginBtn"><a style={{cursor: "pointer"}}><i className="fa fa-sign-in-alt" style={{marginRight: "5px"}}></i>Log In</a></li>
+              </ul>      
+          </nav>
         </header>
   
       </div>
