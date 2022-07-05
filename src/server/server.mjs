b7871bc7231cdae54456dc1a1274fdb6c80ddcfc
Keaton Robinson <keatonr06@gmail.com>
2022-07-04 20:19:39 -0700

    local site now running on https

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -1,10 +1,20 @@
+import https from 'https';
+import fs from 'fs';
+
 import express from 'express';
 // import React from 'react';    not using server side rendering anymore for now...
 // import ReactDOMServer from 'react-dom/server';
 // import App from '../components/App';
 
-const server = express();
-server.use(express.static('dist'));
+const app = express();
+
 // could switch to static file serving, perhaps even with initial react components rendered already and using react's hydrate on the client side
+app.use(express.static('dist'));
+
+const options = {
+    key: fs.readFileSync("./cert/key.pem"),
+    cert: fs.readFileSync("./cert/cert.pem"),
+};
+const server = https.createServer(options, app);
 
 server.listen(4242, () => console.log('Server is running...'));
