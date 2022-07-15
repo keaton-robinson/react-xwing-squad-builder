3dc68409b9aa3fda652a79c79f49c772943e4195
Keaton Robinson <keatonr06@gmail.com>
2022-07-15 12:45:14 -0700

    not hard coding the port number anymore

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -24,4 +24,25 @@ app.use(express.static('dist'));
 
 const server = http.createServer(app);
 
-server.listen(4242, () => console.log('Server is running...'));
+var port = normalizePort(process.env.PORT || '4242');
+
+
+
+server.listen(port, () => console.log(`Server is running on port ${port}...`));
+
+
+function normalizePort(val) {
+	var port = parseInt(val, 10);
+ 
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+ 
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+ 
+	return false;
+ }
