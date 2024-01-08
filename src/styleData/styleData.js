fd474a17adbd8c243030d49809fe506b69379534
Keaton Robinson <keatonr06@gmail.com>
2024-01-08 14:53:34 -0800

    changed export to inline. Comment change

--- src/styleData/styleData.js
+++ src/styleData/styleData.js
@@ -1,4 +1,4 @@
-const DropDownStyles = {
+export const DropDownStyles = {
     wrapper: { fontSize: '0.9rem', width: '100%', maxWidth: '300px' },
     header: { lineHeight: '1.2rem' },
     headerTitle: { marginTop: '2px', marginBottom: '2px', marginRight: '10px', marginLeft: '10px'},
@@ -6,5 +6,3 @@ const DropDownStyles = {
     scrollList: { padding: '5px'},
     listItem: { fontSize: '0.9rem', lineHeight: '1rem', paddingTop: '2px', paddingBottom: '2px', paddingLeft: '15px', paddingRight: '15px'}
 }
-
-export { DropDownStyles }
