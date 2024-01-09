14727793ab6bdcbbf110e75bae7640482dc82ce8
Keaton Robinson <keatonr06@gmail.com>
2024-01-09 15:31:23 -0800

    begining switch to ESM from commonJS

--- webpack.config-dev.js
+++ webpack.config-dev.js
@@ -1,9 +1,9 @@
-const webpack = require('webpack');
-
-const config = require('./webpack.base_config.js'); 
+import webpack from 'webpack'
+ 
+import config from './webpack.base_config.js';
 
 config.plugins.push(new webpack.DefinePlugin({
 	XWING_API_ENDPOINT: JSON.stringify('http://localhost:3000')
 }));
 
-module.exports = config;
+export default config 
--- webpack.config-prod.js
+++ webpack.config-prod.js
@@ -1,6 +1,6 @@
-const webpack = require('webpack');
-
-const config = require('./webpack.base_config.js'); 
+import webpack from 'webpack'
+ 
+import config from './webpack.base_config';
 
 if(!process.env.XWING_API_ENDPOINT){
 	throw new Error("XWING_API_ENDPOINT not set! This must be set for the backend to work.");
@@ -9,4 +9,4 @@ config.plugins.push(new webpack.DefinePlugin({
 	XWING_API_ENDPOINT: JSON.stringify(process.env.XWING_API_ENDPOINT)
 }));
 
-module.exports = config;
+export default config
