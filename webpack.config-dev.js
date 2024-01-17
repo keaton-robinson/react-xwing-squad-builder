5e25ba0cf19b6483fc0b7767d5490d37f983d43f
Keaton Robinson <keatonr06@gmail.com>
2024-01-17 00:35:41 -0800

    added typescript

--- webpack.config-dev.js
+++ webpack.config-dev.js
@@ -2,6 +2,8 @@ const webpack = require('webpack');
 
 const config = require('./webpack.base_config.js'); 
 
+// plugins may not work with devtool option in config
+
 config.plugins.push(new webpack.DefinePlugin({
 	XWING_API_ENDPOINT: JSON.stringify('http://localhost:3000')
 }));
