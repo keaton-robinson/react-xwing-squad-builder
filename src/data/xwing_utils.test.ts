9402d36d5d54770570e23581c25d3b043f9e6d2e
Keaton Robinson <keaton.robinson@hubley.com>
2024-01-18 01:09:37 -0800

    got my tests working with typescript

--- src/data/xwing_utils.test.ts
+++ src/data/xwing_utils.test.ts
@@ -1,3 +1,4 @@
+import { Faction, Ship, Pilot, PilotRulesText, Upgrade, UpgradeRulesText, Slots } from './xwing_data';
 import * as xwing_utils from './xwing_utils';
 
 describe('isNotNullOrUndefined', () => {
@@ -14,57 +15,58 @@ describe('isNotNullOrUndefined', () => {
 })
 
 describe('getShipBaseSize', () => {
-    const hugeShip = { huge: !0, large: 0, medium: 0 }
-    const largeShip = { huge: 0, large: !0, medium: 0 }
-    const mediumShip = { huge: 0, large: 0, medium: !0 }
-    const smallShip = { huge: 0, large: 0, medium: 0 };
-    const dumbShipWithHugeAndLargeMarkedTrue = { huge: !0, large: !0, medium: 0 }
+    const hugeShip: Partial<Ship> = { huge: true, large: false, medium: false };
+    const largeShip: Partial<Ship> = { huge: false, large: true, medium: false };
+    const mediumShip: Partial<Ship> = { huge: false, large: false, medium: true };
+    const smallShip: Partial<Ship> = { huge: false, large: false, medium: false };
+    const dumbShipWithHugeAndLargeMarkedTrue: Partial<Ship> = { huge: true, large: true, medium: false };
 
-    it.each([
-        [hugeShip, xwing_utils.shipBaseSizes.Huge], // data uses !0 rather than true to represent ship size
-        [largeShip, xwing_utils.shipBaseSizes.Large],
-        [mediumShip, xwing_utils.shipBaseSizes.Medium],
-        [smallShip, xwing_utils.shipBaseSizes.Small],
-        [dumbShipWithHugeAndLargeMarkedTrue, xwing_utils.shipBaseSizes.Huge] // not a huge fan of this behavior, but that's what it does right now. I don't think anything really expects this
+    it.each<[Partial<Ship>, xwing_utils.ShipBaseSize]>([
+        [hugeShip, 'Huge'],
+        [largeShip, 'Large'],
+        [mediumShip, 'Medium'],
+        [smallShip, 'Small'],
+        [dumbShipWithHugeAndLargeMarkedTrue, 'Huge']
     ])("when ship is `%s`, expect `%s`", (ship, expectedResult) => {
-        expect(xwing_utils.getShipBaseSize(ship)).toBe(expectedResult)
+        expect(xwing_utils.getShipBaseSize(ship as Ship)).toBe(expectedResult);
     });
-})
+});
+
 
 describe('getUpgradeCost', () => {
     it('when no point cost or points array, throws exception', () => {
         const upgradeWithNoPointCostAtAll = { points: undefined, pointsarray: undefined };
 
-        expect(() => xwing_utils.getUpgradeCost(upgradeWithNoPointCostAtAll, null)).toThrow('Error calculating points on upgrade');
+        expect(() => xwing_utils.getUpgradeCost(upgradeWithNoPointCostAtAll as Upgrade, null)).toThrow('Error calculating points on upgrade');
     })
     it('when static point cost defined, returns static point cost', () => {
         const upgradeWithStaticPointCost = { points:  8 }
         const expectedReturnValue = 8;
 
-        expect(xwing_utils.getUpgradeCost(upgradeWithStaticPointCost, null)).toBe(expectedReturnValue);
+        expect(xwing_utils.getUpgradeCost(upgradeWithStaticPointCost as Upgrade, null)).toBe(expectedReturnValue);
     })
     describe('variable point cost based on pilot skill', () => {
         const upgradeWithVariableInitPoints = {
             variableinit: !0,
             pointsarray: [0,1,2,3,4,5,6,7,8,9,10]
         };
-        const pilotWithSkill0 = { skill: 0};
-        const pilotWithSkill1 = { skill: 1};
-        const pilotWithSkill2 = { skill: 2}
-        const pilotWithSkill3 = { skill: 3}
-        const pilotWithSkill4 = { skill: 4}
-        const pilotWithSkill5 = { skill: 5}
-        const pilotWithSkill6 = { skill: 6}
-        const pilotWithSkillTen = { skill: 10 };
+        const pilotWithSkill0: Partial<Pilot> = { skill: 0};
+        const pilotWithSkill1: Partial<Pilot> = { skill: 1};
+        const pilotWithSkill2: Partial<Pilot> = { skill: 2}
+        const pilotWithSkill3: Partial<Pilot> = { skill: 3}
+        const pilotWithSkill4: Partial<Pilot> = { skill: 4}
+        const pilotWithSkill5: Partial<Pilot> = { skill: 5}
+        const pilotWithSkill6: Partial<Pilot> = { skill: 6}
+        const pilotWithSkillTen: Partial<Pilot> = { skill: 10 };
 
 
-        it.each([
+        it.each<[Partial<Pilot>, number]>([
             [pilotWithSkill0, 0],
             [pilotWithSkill1, 1],
             [pilotWithSkill6, 6],
             [pilotWithSkillTen, 10]
         ])("when pilot is %s, return point cost from index %s", (pilot, returnValue) => {
-            expect(xwing_utils.getUpgradeCost(upgradeWithVariableInitPoints, pilot)).toBe(returnValue);
+            expect(xwing_utils.getUpgradeCost(upgradeWithVariableInitPoints as Upgrade, pilot as xwing_utils.SelectedPilot)).toBe(returnValue);
         });
     })
     describe('variable point cost based on ship base size', () => {
@@ -86,7 +88,7 @@ describe('getUpgradeCost', () => {
             [pilotWithMediumShip, 6],
             [pilotWithSmallShip, 4]
         ])("when pilot has %s ship, returns point cost %s", (pilot, returnValue) => {
-            expect(xwing_utils.getUpgradeCost(upgradeWithVariableBasePoints, pilot)).toBe(returnValue);
+            expect(xwing_utils.getUpgradeCost(upgradeWithVariableBasePoints as Upgrade, pilot as xwing_utils.SelectedPilot)).toBe(returnValue);
         });
     });
     
@@ -106,7 +108,7 @@ describe('getUpgradeCost', () => {
             [pilotWithAgility2, 6],
             [pilotWithAgility3, 9]
         ])("when pilot's ship has agility %s, returns point cost %s", (pilot, returnValue) => {
-            expect(xwing_utils.getUpgradeCost(upgradeWithVariableAgilityPoints, pilot)).toBe(returnValue);
+            expect(xwing_utils.getUpgradeCost(upgradeWithVariableAgilityPoints as Upgrade, pilot as xwing_utils.SelectedPilot)).toBe(returnValue);
         });
     });
     
