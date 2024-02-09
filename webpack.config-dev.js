47a6af368286e8d798cdde97222f1f32a6be74c0
Keaton Robinson <keatonr06@gmail.com>
2024-02-08 20:09:49 -0800

    fixed strict mode error on LoginModal

--- webpack.config-dev.js
+++ webpack.config-dev.js
@@ -5,7 +5,7 @@ const config = require('./webpack.base_config.js');
 // plugins may not work with devtool option in config
 
 config.plugins.push(new webpack.DefinePlugin({
-	XWING_API_ENDPOINT: JSON.stringify('http://localhost:3000')
+	XWING_API_ENDPOINT: JSON.stringify('http://localhost:4567')
 }));
 
 module.exports = config;
