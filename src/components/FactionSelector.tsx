3f50abaf01857811c27c69ad4d92131a09664ddd
Keaton Robinson <keatonr06@gmail.com>
2024-04-23 16:21:34 -0700

    separated app.jsx into separate components

---
+++ src/components/FactionSelector.tsx
@@ -0,0 +1,40 @@
+import React, { useContext, useState } from 'react';
+import * as xwingData from '../data/xwing_data';
+import HeaderComponent from './HeaderComponent';
+import SquadBuilderCpt from './SquadBuilderCpt';
+import { useModalSetter } from './modals/ModalContext';
+
+const factionsOrdered: xwingData.Faction[] = [
+    xwingData.factionNames["Rebel Alliance"],
+    xwingData.factionNames["Galactic Empire"],
+    xwingData.factionNames["Scum and Villainy"],
+    xwingData.factionNames.Resistance,
+    xwingData.factionNames["First Order"],
+    xwingData.factionNames["Galactic Republic"],
+    xwingData.factionNames["Separatist Alliance"]
+  ];
+
+const FactionSelector: React.FC = () => {
+    const [selectedFaction, setSelectedFaction] = useState<keyof typeof xwingData.factionNames>(xwingData.factionNames['Rebel Alliance']);
+    const setModal = useModalSetter();
+    return (
+        <>
+          <HeaderComponent
+            factions={factionsOrdered}
+            selectedFaction={selectedFaction}
+            onClick={setSelectedFaction}
+          />
+          <main>
+            {factionsOrdered.map(faction => (
+              <SquadBuilderCpt key={faction}
+                selectedFaction={selectedFaction}
+                faction={faction}
+                setModal={setModal}
+              />
+            ))}
+          </main>
+        </>
+    );
+}
+
+export default FactionSelector;
--- src/server/server.mjs
+++ src/server/server.mjs
@@ -1,36 +1,10 @@
 import http from 'http';
-// import https from 'https';
-// import fs from 'fs';
-// import compression from 'compression'
-// import helmet from 'helmet';
-
 import express from 'express';
-// import React from 'react';    not using server side rendering anymore for now...
-// import ReactDOMServer from 'react-dom/server';
-// import App from '../components/App';
 
 const app = express();
-// app.use(compression());   //TODO: get these turned back on
-// app.use(helmet());
-
 app.use(express.static('dist'));
-
-
-// old: going to just use Heroku out of the box SSL for the moment. Technically should add https redirects and use helmet to avoid https downgrades and stuff
-// TODO: ponder doing something with Amazon EC2 since we aren't using Heroku anymore...  
-// for local https development
-// const options = {
-//     key: fs.readFileSync("./cert/key.pem"),
-//     cert: fs.readFileSync("./cert/cert.pem"),
-// };
-//const server = https.createServer(options, app);
-
 const server = http.createServer(app);
-
 var port = normalizePort(process.env.PORT || '4242');
-
-
-
 server.listen(port, () => console.log(`Server is running on port ${port}...`));
 
 
