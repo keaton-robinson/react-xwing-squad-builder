98714eaa85aa0a5b2ed026914fcb5805869e4224
Keaton Robinson <keaton.robinson@hubley.com>
2024-04-23 10:58:36 -0700

    made saveAsModal a function component

--- src/components/modals/SaveAsModal.tsx
+++ src/components/modals/SaveAsModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState}  from 'react';
 
 
 interface SaveAsModalProps {
@@ -9,34 +9,30 @@ interface SaveAsModalProps {
 interface SaveAsModalState {
     squadName: string
 }
-export default class SaveAsModal extends React.Component<SaveAsModalProps, SaveAsModalState> {
+
+const SaveAsModal:React.FC<SaveAsModalProps> = (props) =>  {
+    const [state, setState] = useState({ squadName: props.squadName});
+
+    const squadNameChanged = (event) => {
+        setState({squadName: event.target.value});
+    }
+
+    const saveAsClicked = () => {
+        props.saveSquad(state.squadName);
+    }
     
-    constructor(props) {
-        super(props);
-
-        this.state = { squadName: this.props.squadName};
-    }
-
-    squadNameChanged = (event) => {
-        this.setState({squadName: event.target.value});
-    }
-
-    saveAsClicked = () => {
-        this.props.saveSquad(this.state.squadName);
-    }
-
-    render() {
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    Save your squad?
-                </div>
-                <span>Squad Name: <input type="text" value={this.state.squadName} onChange={this.squadNameChanged} maxLength={30}/></span>
-                <hr/>
-                <div style={{ textAlign: "right"}}>
-                    <button className="btn-primary" onClick={this.saveAsClicked}><i className="fa-solid fa-save"></i> Save</button>
-                </div>
+                Save your squad?
             </div>
-        );
-    }
+            <span>Squad Name: <input type="text" value={state.squadName} onChange={squadNameChanged} maxLength={30}/></span>
+            <hr/>
+            <div style={{ textAlign: "right"}}>
+                <button className="btn-primary" onClick={saveAsClicked}><i className="fa-solid fa-save"></i> Save</button>
+            </div>
+        </div>
+    );
 }
+
+export default SaveAsModal;
