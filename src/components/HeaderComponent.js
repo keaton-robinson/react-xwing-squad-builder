95e6f988d9b5e970b81229308606295cb69b0a55
Keaton Robinson <keatonr06@gmail.com>
2024-04-23 16:47:10 -0700

    cleaned up headercomponent

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,11 +2,12 @@ import React from 'react';
 import FactionCpt from './FactionCpt';
 import LoginModal from './modals/LoginModal';
 import RegisterModal from './modals/RegisterModal';
-import { UserContext } from '../contexts/UserContext'
+import { UserContext, useUserContext } from '../contexts/UserContext'
 import { useModalSetter } from '../contexts/ModalContext';
 
 export default function HeaderComponent(props)  {
   const setModal = useModalSetter();
+  const userContext = useUserContext();
 
   const showLogin = () => {
     setModal({
@@ -22,31 +23,26 @@ export default function HeaderComponent(props)  {
     })
   }
 
-  return (
-    <UserContext.Consumer>
-      { (userContext) => {
-        return (<header id="navbar">
-          <nav id="factionChooser">
-            <ul>
-            {props.factions.map(faction => (
-                <FactionCpt faction={ faction } key={ faction } selectedFaction={ props.selectedFaction }
-                  onClick={props.onClick}
-                />
-              ))}
-            </ul>
-          </nav>
-          <nav id="navLinks">
-              <ul>
-                <li><a href="https://github.com/keaton-robinson/react-xwing-squad-builder">GitHub Repository</a></li>
-                { userContext.user ?
-                    <li id="logoutBtn" onClick={userContext.logout}><a style={{cursor: "pointer"}}><i className="fa fa-sign-in-alt" style={{marginRight: "5px"}}></i>Log Out</a></li>
-                  :
-                    <li id="loginBtn" onClick={showLogin}><a style={{cursor: "pointer"}}><i className="fa fa-sign-in-alt" style={{marginRight: "5px"}}></i>Log In</a></li>
-                }
-              </ul>      
-          </nav>
-        </header>);
-      }}   
-    </UserContext.Consumer>
-  );
+  return <header id="navbar">
+    <nav id="factionChooser">
+      <ul>
+      { props.factions.map(faction => (
+          <FactionCpt faction={ faction } key={ faction } selectedFaction={ props.selectedFaction }
+            onClick={props.onClick}
+          />
+        ))
+      }
+      </ul>
+    </nav>
+    <nav id="navLinks">
+        <ul>
+          <li><a href="https://github.com/keaton-robinson/react-xwing-squad-builder">GitHub Repository</a></li>
+          { userContext.user ?
+              <li id="logoutBtn" onClick={userContext.logout}><a style={{cursor: "pointer"}}><i className="fa fa-sign-in-alt" style={{marginRight: "5px"}}></i>Log Out</a></li>
+            :
+              <li id="loginBtn" onClick={showLogin}><a style={{cursor: "pointer"}}><i className="fa fa-sign-in-alt" style={{marginRight: "5px"}}></i>Log In</a></li>
+          }
+        </ul>      
+    </nav>
+  </header>
 }
