51f186106cabf3404cabdf1ed4df0c90c1c71a9d
Keaton Robinson <keatonr06@gmail.com>
2024-04-29 17:14:30 -0700

    converted statblock and upgrades cpt to functional cpts

--- src/components/StatBlockCpt.tsx
+++ src/components/StatBlockCpt.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-export default class StatBlockCpt extends React.Component<{
+interface StatBlockCptProps {
   pilot;
   ship;
   upgrade;
   pilotAfterUpgrades;
-}> {
-  renderEffectiveStat = (baseStat, statAfterUpgrades) => {
+}
+
+const StatBlockCpt: React.FC<StatBlockCptProps> = (props) => {
+  const renderEffectiveStat = (baseStat, statAfterUpgrades) => {
     return (
       <span>
         {baseStat ? baseStat : 0}{" "}
@@ -17,216 +19,210 @@ export default class StatBlockCpt extends React.Component<{
     );
   };
 
-  validateInputAndGetCardToShow = () => {
-    if (!(this.props.pilot || this.props.ship || this.props.upgrade)) {
+  const validateInputAndGetCardToShow = () => {
+    if (!(props.pilot || props.ship || props.upgrade)) {
       throw new Error(
         `StatBlockCpt requires at least one of pilot, ship, or upgrade props to be set`,
       );
-    } else if (this.props.pilot) {
-      if (this.props.ship || this.props.upgrade) {
+    } else if (props.pilot) {
+      if (props.ship || props.upgrade) {
         const error: any = new Error(
           `StatBlockCpt doesn't allow ship or upgrade prop to be set when pilot prop is set.`,
         );
-        error.shipVal = this.props.ship;
-        error.upgradeVal = this.props.upgrade;
-        error.pilotVal = this.props.pilot;
+        error.shipVal = props.ship;
+        error.upgradeVal = props.upgrade;
+        error.pilotVal = props.pilot;
         throw error;
       }
-      return this.props.pilot.pilotShip;
-    } else if (this.props.ship && this.props.upgrade) {
+      return props.pilot.pilotShip;
+    } else if (props.ship && props.upgrade) {
       const error: any = new Error(
         `StatBlockCpt doesn't allow ship and upgrade prop to both be set.`,
       );
-      error.shipVal = this.props.ship;
-      error.upgradeVal = this.props.upgrade;
+      error.shipVal = props.ship;
+      error.upgradeVal = props.upgrade;
       throw error;
     } else {
-      return this.props.ship ? this.props.ship : this.props.upgrade;
+      return props.ship ? props.ship : props.upgrade;
     }
   };
 
-  render() {
-    let shipOrUpgrade = this.validateInputAndGetCardToShow();
-    const pilot = this.props.pilot;
-    const pilotAfterUpgrades = this.props.pilotAfterUpgrades;
-    const shipAfterUpgrades = pilotAfterUpgrades?.pilotShip;
+  let shipOrUpgrade = validateInputAndGetCardToShow();
+  const pilot = props.pilot;
+  const pilotAfterUpgrades = props.pilotAfterUpgrades;
+  const shipAfterUpgrades = pilotAfterUpgrades?.pilotShip;
 
-    return (
-      <table className="info-stats">
-        <tbody>
-          {shipOrUpgrade.attack || shipAfterUpgrades?.attack ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-frontarc header-attack"></i>
-              </td>
-              <td className="info-attack">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.attack,
-                  shipAfterUpgrades?.attack,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.attackf || shipAfterUpgrades?.attackf ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-fullfrontarc header-attack"></i>
-              </td>
-              <td className="info-attack">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.attackf,
-                  shipAfterUpgrades?.attackf,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.attackb || shipAfterUpgrades?.attackb ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-reararc header-attack"></i>
-              </td>
-              <td className="info-attack">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.attackb,
-                  shipAfterUpgrades?.attackb,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.attackl || shipAfterUpgrades?.attackl ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-leftarc header-attack"></i>
-              </td>
-              <td className="info-attack">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.attackl,
-                  shipAfterUpgrades?.attackl,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.attackr || shipAfterUpgrades?.attackr ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-rightarc header-attack"></i>
-              </td>
-              <td className="info-attack">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.attackr,
-                  shipAfterUpgrades?.attackr,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.attackt || shipAfterUpgrades?.attackt ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-singleturretarc header-attack"></i>
-              </td>
-              <td className="info-attack">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.attackt,
-                  shipAfterUpgrades?.attackt,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.attackdt || shipAfterUpgrades?.attackdt ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-doubleturretarc header-attack"></i>
-              </td>
-              <td className="info-attack">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.attackdt,
-                  shipAfterUpgrades?.attackdt,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.attackbull || shipAfterUpgrades?.attackbull ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-bullseyearc header-attack"></i>
-              </td>
-              <td className="info-attack">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.attackbull,
-                  shipAfterUpgrades?.attackbull,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.agility || shipAfterUpgrades?.agility ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-agility header-stat header-agility"></i>
-              </td>
-              <td className="info-agility">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.agility,
-                  shipAfterUpgrades?.agility,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.hull || shipAfterUpgrades?.hull ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-hull header-stat header-hull"></i>
-              </td>
-              <td className="info-hull">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.hull,
-                  shipAfterUpgrades?.hull,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {shipOrUpgrade.shields || shipAfterUpgrades?.shields ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font xwing-miniatures-font-shield header-stat header-shield"></i>
-              </td>
-              <td className="info-shield">
-                {this.renderEffectiveStat(
-                  shipOrUpgrade.shields,
-                  shipAfterUpgrades?.shields,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {pilot?.force ||
-          shipOrUpgrade.force ||
-          pilotAfterUpgrades?.force + shipAfterUpgrades?.force ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font header-force xwing-miniatures-font-forcecharge"></i>
-              </td>
-              <td className="info-force">
-                {this.renderEffectiveStat(
-                  pilot?.force || shipOrUpgrade.force,
-                  pilotAfterUpgrades?.force + shipAfterUpgrades?.force,
-                )}
-              </td>
-            </tr>
-          ) : null}
-          {pilot?.charge || shipOrUpgrade.charge ? (
-            <tr className="stat-icon">
-              <td>
-                <i className="xwing-miniatures-font header-charge xwing-miniatures-font-charge"></i>
-              </td>
-              <td className="info-charge">
-                {this.renderEffectiveStat(
-                  pilot?.charge || shipOrUpgrade.charge,
-                  null,
-                )}
-              </td>
-            </tr>
-          ) : null}
-        </tbody>
-      </table>
-    );
-  }
-}
+  return (
+    <table className="info-stats">
+      <tbody>
+        {shipOrUpgrade.attack || shipAfterUpgrades?.attack ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-frontarc header-attack"></i>
+            </td>
+            <td className="info-attack">
+              {renderEffectiveStat(
+                shipOrUpgrade.attack,
+                shipAfterUpgrades?.attack,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.attackf || shipAfterUpgrades?.attackf ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-fullfrontarc header-attack"></i>
+            </td>
+            <td className="info-attack">
+              {renderEffectiveStat(
+                shipOrUpgrade.attackf,
+                shipAfterUpgrades?.attackf,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.attackb || shipAfterUpgrades?.attackb ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-reararc header-attack"></i>
+            </td>
+            <td className="info-attack">
+              {renderEffectiveStat(
+                shipOrUpgrade.attackb,
+                shipAfterUpgrades?.attackb,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.attackl || shipAfterUpgrades?.attackl ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-leftarc header-attack"></i>
+            </td>
+            <td className="info-attack">
+              {renderEffectiveStat(
+                shipOrUpgrade.attackl,
+                shipAfterUpgrades?.attackl,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.attackr || shipAfterUpgrades?.attackr ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-rightarc header-attack"></i>
+            </td>
+            <td className="info-attack">
+              {renderEffectiveStat(
+                shipOrUpgrade.attackr,
+                shipAfterUpgrades?.attackr,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.attackt || shipAfterUpgrades?.attackt ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-singleturretarc header-attack"></i>
+            </td>
+            <td className="info-attack">
+              {renderEffectiveStat(
+                shipOrUpgrade.attackt,
+                shipAfterUpgrades?.attackt,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.attackdt || shipAfterUpgrades?.attackdt ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-doubleturretarc header-attack"></i>
+            </td>
+            <td className="info-attack">
+              {renderEffectiveStat(
+                shipOrUpgrade.attackdt,
+                shipAfterUpgrades?.attackdt,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.attackbull || shipAfterUpgrades?.attackbull ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-bullseyearc header-attack"></i>
+            </td>
+            <td className="info-attack">
+              {renderEffectiveStat(
+                shipOrUpgrade.attackbull,
+                shipAfterUpgrades?.attackbull,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.agility || shipAfterUpgrades?.agility ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-agility header-stat header-agility"></i>
+            </td>
+            <td className="info-agility">
+              {renderEffectiveStat(
+                shipOrUpgrade.agility,
+                shipAfterUpgrades?.agility,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.hull || shipAfterUpgrades?.hull ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-hull header-stat header-hull"></i>
+            </td>
+            <td className="info-hull">
+              {renderEffectiveStat(shipOrUpgrade.hull, shipAfterUpgrades?.hull)}
+            </td>
+          </tr>
+        ) : null}
+        {shipOrUpgrade.shields || shipAfterUpgrades?.shields ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font xwing-miniatures-font-shield header-stat header-shield"></i>
+            </td>
+            <td className="info-shield">
+              {renderEffectiveStat(
+                shipOrUpgrade.shields,
+                shipAfterUpgrades?.shields,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {pilot?.force ||
+        shipOrUpgrade.force ||
+        pilotAfterUpgrades?.force + shipAfterUpgrades?.force ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font header-force xwing-miniatures-font-forcecharge"></i>
+            </td>
+            <td className="info-force">
+              {renderEffectiveStat(
+                pilot?.force || shipOrUpgrade.force,
+                pilotAfterUpgrades?.force + shipAfterUpgrades?.force,
+              )}
+            </td>
+          </tr>
+        ) : null}
+        {pilot?.charge || shipOrUpgrade.charge ? (
+          <tr className="stat-icon">
+            <td>
+              <i className="xwing-miniatures-font header-charge xwing-miniatures-font-charge"></i>
+            </td>
+            <td className="info-charge">
+              {renderEffectiveStat(pilot?.charge || shipOrUpgrade.charge, null)}
+            </td>
+          </tr>
+        ) : null}
+      </tbody>
+    </table>
+  );
+};
+
+export default StatBlockCpt;
--- src/components/UpgradesCpt.tsx
+++ src/components/UpgradesCpt.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { sloticon } from "../data/xwing_data";
 
-import * as xwingData from "../data/xwing_data";
+interface UpgradesCptProps {
+  pilots: any;
+}
 
-export default class UpgradesCpt extends React.Component<{ pilots: any }> {
-  getSlotMarkup = (slot) => {
+const UpgradesCpt: React.FC<UpgradesCptProps> = (props) => {
+  const getSlotMarkup = (slot) => {
     let slotName = slot[0];
     let numSlotsPerPilot = slot[1].numSlotsPerPilot;
     let upgradeIsAvailableToAllPilots =
-      slot[1].numPilots === this.props.pilots.length; //slot count is slot[1].numPilots
+      slot[1].numPilots === props.pilots.length; //slot count is slot[1].numPilots
     let innerHtml = "";
     for (let i = 0; i < numSlotsPerPilot; i++) {
-      innerHtml += xwingData.sloticon[slotName];
+      innerHtml += sloticon[slotName];
     }
 
     if (!upgradeIsAvailableToAllPilots) {
@@ -24,39 +27,37 @@ export default class UpgradesCpt extends React.Component<{ pilots: any }> {
     );
   };
 
-  render() {
-    const slotCountAcrossPilots = {};
-    //example: {'Torpedo':{numPerPilot: 2, numPilots: 4} 'Astromech':{numPerPilot: 2, numPilots: 4}, 'Talent': {numPerPilot: 2, numPilots: 4}, 'Force': {numPerPilot: 2, numPilots: 4}}
+  const slotCountAcrossPilots = {};
+  //example: {'Torpedo':{numPerPilot: 2, numPilots: 4} 'Astromech':{numPerPilot: 2, numPilots: 4}, 'Talent': {numPerPilot: 2, numPilots: 4}, 'Force': {numPerPilot: 2, numPilots: 4}}
 
-    //grrr...having to do this sort of thing in the render method seems inefficient....wonder if it could go somewhere else...
-    for (const pilot of this.props.pilots) {
-      let slotCountForCurrentPilot = {};
-      for (const slot of pilot.slots) {
-        if (!slotCountForCurrentPilot[slot]) {
-          slotCountForCurrentPilot[slot] = 1;
-        } else {
-          slotCountForCurrentPilot[slot]++;
-        }
-      }
-
-      for (const slotCount of Object.entries(slotCountForCurrentPilot)) {
-        if (!slotCountAcrossPilots[slotCount[0]]) {
-          slotCountAcrossPilots[slotCount[0]] = {
-            numPilots: 1,
-            numSlotsPerPilot: slotCountForCurrentPilot[slotCount[0]],
-          };
-        } else {
-          slotCountAcrossPilots[slotCount[0]].numPilots++;
-        }
+  //grrr...having to do this sort of thing in the render method seems inefficient....wonder if it could go somewhere else...
+  for (const pilot of props.pilots) {
+    let slotCountForCurrentPilot = {};
+    for (const slot of pilot.slots) {
+      if (!slotCountForCurrentPilot[slot]) {
+        slotCountForCurrentPilot[slot] = 1;
+      } else {
+        slotCountForCurrentPilot[slot]++;
       }
     }
 
-    return (
-      <span>
-        {Object.entries(slotCountAcrossPilots).map((slot) =>
-          this.getSlotMarkup(slot),
-        )}
-      </span>
-    );
+    for (const slotCount of Object.entries(slotCountForCurrentPilot)) {
+      if (!slotCountAcrossPilots[slotCount[0]]) {
+        slotCountAcrossPilots[slotCount[0]] = {
+          numPilots: 1,
+          numSlotsPerPilot: slotCountForCurrentPilot[slotCount[0]],
+        };
+      } else {
+        slotCountAcrossPilots[slotCount[0]].numPilots++;
+      }
+    }
   }
-}
+
+  return (
+    <span>
+      {Object.entries(slotCountAcrossPilots).map((slot) => getSlotMarkup(slot))}
+    </span>
+  );
+};
+
+export default UpgradesCpt;
