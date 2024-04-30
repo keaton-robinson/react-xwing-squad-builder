a7965d721597b1015b7d7e38b607652b1b4f5e4f
Keaton Robinson <keatonr06@gmail.com>
2024-04-29 17:08:41 -0700

    converted shipUpgradeCpt to function component

--- src/components/ShipUpgradeCpt.tsx
+++ src/components/ShipUpgradeCpt.tsx
@@ -1,136 +1,135 @@
-import React from "react";
+import React, { useRef } from "react";
 import * as xwingData from "../data/xwing_data";
 import * as xwingUtils from "../data/xwing_utils";
 import { Dropdown } from "@keatonr06/reactjs-dropdown-component";
 import { DropDownStyles } from "../styleData/styleData";
+import { SelectedPilot, SelectedUpgrade } from "../data/xwing_utils";
+import { Upgrade } from "../data/xwing_data";
 
-export default class ShipUpgradeCpt extends React.Component<any> {
-  private ddlSelectedUpgradeRef;
+interface ShipUpgradeCptProps {
+  upgradeSlot: SelectedUpgrade;
+  changeUpgrade: (
+    upgradeSlot: SelectedUpgrade,
+    newlySelectedUpgrade: Upgrade,
+    pilot: SelectedPilot,
+  ) => void;
+  pilot: SelectedPilot;
+  squad: SelectedPilot[];
+  onRecordMouseEnter: (upgrade: Upgrade) => void;
+}
 
-  constructor(props) {
-    super(props);
-    this.ddlSelectedUpgradeRef = React.createRef();
-  }
+const ShipUpgradeCpt: React.FC<ShipUpgradeCptProps> = (props) => {
+  const ddlSelectedUpgradeRef = useRef(null);
 
-  upgradeAlreadySelectedOnADifferentSlot = (upgrade) => {
-    return this.props.pilot.selectedUpgrades.find(
+  const upgradeAlreadySelectedOnADifferentSlot = (upgrade) => {
+    return props.pilot.selectedUpgrades.find(
       (selUpgrade) =>
         selUpgrade.selectedUpgradeId === upgrade.id &&
-        selUpgrade.key !== this.props.upgradeSlot.key,
+        selUpgrade.key !== props.upgradeSlot.key,
     );
   };
 
-  getAvailableUpgrades = (squadContainsAnotherSolitaryCardForThisSlot) => {
+  const getAvailableUpgrades = (
+    squadContainsAnotherSolitaryCardForThisSlot,
+  ) => {
     if (squadContainsAnotherSolitaryCardForThisSlot) {
       //if another ship has selected a 'solitary' upgrade for this slot, this ship cannot select an upgrade for this slot (ex. tactical relays)
       return null;
     }
     const matchingSlots = [];
-    if (this.props.upgradeSlot.slot === xwingData.slots.HardpointShip.key) {
+    if (props.upgradeSlot.slot === xwingData.slots.HardpointShip.key) {
       // a "hardpointship" upgrade means the slot can accept a cannon, missile, or torpedo upgrade
       matchingSlots.push(xwingData.slots.Cannon.key);
       matchingSlots.push(xwingData.slots.Missile.key);
       matchingSlots.push(xwingData.slots.Torpedo.key);
     } else {
       // otherwise, treat it as a normal upgrade
-      matchingSlots.push(this.props.upgradeSlot.slot);
+      matchingSlots.push(props.upgradeSlot.slot);
     }
 
     return xwingData.upgrades.filter(
       (upgrade) =>
-        matchingSlots.includes(upgrade.slot) && // upgrade.slot === this.props.upgradeSlot.slot
+        matchingSlots.includes(upgrade.slot) && // upgrade.slot === props.upgradeSlot.slot
         (!xwingUtils.maxPilotOrUpgradeReached(
           upgrade,
-          this.props.squad,
+          props.squad,
           xwingData.upgrades,
         ) ||
-          this.props.upgradeSlot.selectedUpgradeId === upgrade.id) &&
+          props.upgradeSlot.selectedUpgradeId === upgrade.id) &&
         xwingUtils.isUpgradeAllowed(
-          this.props.upgradeSlot,
+          props.upgradeSlot,
           upgrade,
-          this.props.pilot,
-          this.props.squad,
+          props.pilot,
+          props.squad,
           xwingData.upgrades,
         ) &&
-        !this.upgradeAlreadySelectedOnADifferentSlot(upgrade),
+        !upgradeAlreadySelectedOnADifferentSlot(upgrade),
     );
   };
 
-  handleUpgradeSelection = (selectedUpgrade) => {
-    if (selectedUpgrade.value !== this.props.upgradeSlot.selectedUpgradeId) {
+  const handleUpgradeSelection = (selectedUpgrade) => {
+    if (selectedUpgrade.value !== props.upgradeSlot.selectedUpgradeId) {
       const newlySelectedUpgrade = xwingData.upgrades.find(
         (upgrade) => upgrade.id === selectedUpgrade.value,
       );
-      this.props.changeUpgrade(
-        this.props.upgradeSlot,
-        newlySelectedUpgrade,
-        this.props.pilot,
-      );
+      props.changeUpgrade(props.upgradeSlot, newlySelectedUpgrade, props.pilot);
     }
   };
 
-  handleMouseEnter = (upgradeDropDownItem) => {
+  const handleMouseEnter = (upgradeDropDownItem) => {
     if (upgradeDropDownItem.value) {
-      this.props.onRecordMouseEnter(upgradeDropDownItem.upgradeRecord);
+      props.onRecordMouseEnter(upgradeDropDownItem.upgradeRecord);
     }
   };
 
-  // componentDidUpdate = (prevProps) => {
-  //     // TODO: fix the custom drop downs so I don't have to directly modify a child component like this
-  //     //the custom dropdowns don't automatically update their selected item or "title" on re-renders, sadly
-  //     let current = this.ddlSelectedUpgradeRef.current;
-  //     if(current.state.selectedItem && current.state.selectedItem.value != this.props.upgradeSlot.selectedUpgradeId){
-  //         current.selectSingleItem({value: this.props.upgradeSlot.selectedUpgradeId});
-  //     }
-  // }
+  const squadContainsAnotherSolitaryCardForThisSlot =
+    xwingUtils.squadContainsAnotherSolitaryCardForThisSlot(
+      props.upgradeSlot,
+      props.squad,
+      xwingData.upgrades,
+    );
+  const availableUpgrades = getAvailableUpgrades(
+    squadContainsAnotherSolitaryCardForThisSlot,
+  ).sort(
+    (upgrade1, upgrade2) =>
+      xwingUtils.getUpgradeCost(upgrade1, props.pilot) -
+      xwingUtils.getUpgradeCost(upgrade2, props.pilot),
+  );
+  const upgradesForCustomDropdown: {
+    value: number;
+    label: string;
+    upgradeRecord?: xwingData.Upgrade;
+  }[] = availableUpgrades.map((availUpgrade) => ({
+    label:
+      availUpgrade.name +
+      " (" +
+      xwingUtils.getUpgradeCost(availUpgrade, props.pilot) +
+      ")",
+    value: availUpgrade.id,
+    upgradeRecord: availUpgrade,
+  }));
+  upgradesForCustomDropdown.unshift({
+    value: null,
+    label: `No ${xwingData.slots[props.upgradeSlot.slot].displayName} Upgrade`,
+  }); // needed so that people can remove upgrades
 
-  render() {
-    const squadContainsAnotherSolitaryCardForThisSlot =
-      xwingUtils.squadContainsAnotherSolitaryCardForThisSlot(
-        this.props.upgradeSlot,
-        this.props.squad,
-        xwingData.upgrades,
-      );
-    const availableUpgrades = this.getAvailableUpgrades(
-      squadContainsAnotherSolitaryCardForThisSlot,
-    ).sort(
-      (upgrade1, upgrade2) =>
-        xwingUtils.getUpgradeCost(upgrade1, this.props.pilot) -
-        xwingUtils.getUpgradeCost(upgrade2, this.props.pilot),
-    );
-    const upgradesForCustomDropdown: {
-      value: number;
-      label: string;
-      upgradeRecord?: xwingData.Upgrade;
-    }[] = availableUpgrades.map((availUpgrade) => ({
-      label:
-        availUpgrade.name +
-        " (" +
-        xwingUtils.getUpgradeCost(availUpgrade, this.props.pilot) +
-        ")",
-      value: availUpgrade.id,
-      upgradeRecord: availUpgrade,
-    }));
-    upgradesForCustomDropdown.unshift({
-      value: null,
-      label: `No ${xwingData.slots[this.props.upgradeSlot.slot].displayName} Upgrade`,
-    }); // needed so that people can remove upgrades
-    return (
-      <Dropdown
-        name="selectUpgrade"
-        titleSingular="Upgrade"
-        ref={this.ddlSelectedUpgradeRef}
-        title={upgradesForCustomDropdown[0].label}
-        list={upgradesForCustomDropdown}
-        onChange={this.handleUpgradeSelection}
-        styles={DropDownStyles}
-        onMouseEnter={this.handleMouseEnter}
-        immutable={
-          this.props.upgradeSlot.parentUpgradeSlotKey ||
-          squadContainsAnotherSolitaryCardForThisSlot
-        }
-        select={{ value: this.props.upgradeSlot.selectedUpgradeId }}
-      />
-    );
-  }
-}
+  return (
+    <Dropdown
+      name="selectUpgrade"
+      titleSingular="Upgrade"
+      ref={ddlSelectedUpgradeRef}
+      title={upgradesForCustomDropdown[0].label}
+      list={upgradesForCustomDropdown}
+      onChange={handleUpgradeSelection}
+      styles={DropDownStyles}
+      onMouseEnter={handleMouseEnter}
+      immutable={
+        props.upgradeSlot.parentUpgradeSlotKey ||
+        squadContainsAnotherSolitaryCardForThisSlot
+      }
+      select={{ value: props.upgradeSlot.selectedUpgradeId }}
+    />
+  );
+};
+
+export default ShipUpgradeCpt;
